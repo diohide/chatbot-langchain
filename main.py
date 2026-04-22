782280bd5dd82d08ab7f665d0a87ce3bd4cc7ce9
Diogo Hideyoshi <diohide12@gmail.com>
2026-04-22 15:23:44 -0300

    feat: add more 2 chains, restaurants and culture

--- main.py
+++ main.py
@@ -13,7 +13,12 @@ class Destino(BaseModel):
     cidade: str = Field("A cidade recomendada para visitar")
     motivo: str = Field("O motivo pelo qual essa cidade é recomendada")
 
-parseador = JsonOutputParser(pydantic_object=Destino)
+class Restaurantes(BaseModel):
+    cidade: str = Field("A cidade recomendada para visitar")
+    restaurantes: str = Field("Uma lista de restaurantes recomendados nessa cidade")
+
+parseador_destino = JsonOutputParser(pydantic_object=Destino)
+parseador_restaurantes = JsonOutputParser(pydantic_object=Restaurantes)
 
 prompt_cidade = PromptTemplate(
     template="""
@@ -21,7 +26,19 @@ prompt_cidade = PromptTemplate(
     {formato_de_saida}
     """,
     input_variables=["interesse"],
-    partial_variables={"formato_de_saida" : parseador.get_format_instructions() }
+    partial_variables={"formato_de_saida" : parseador_destino.get_format_instructions() }
+)
+
+prompt_restaurantes = PromptTemplate(
+    template="""
+    Sugira uma lista de restaurantes na cidade {cidade}.
+    {formato_de_saida}
+    """,
+    partial_variables={"formato_de_saida" : parseador_restaurantes.get_format_instructions() }
+)
+
+prompt_cultura = PromptTemplate(
+    template= "Sugira atividades culturais para fazer na cidade {cidade}."
 )
 
 modelo = ChatOpenAI(
@@ -30,7 +47,11 @@ modelo = ChatOpenAI(
     api_key=api_key
 )
 
-chain = prompt_cidade | modelo | parseador
+chain_1 = prompt_cidade | modelo | parseador_destino
+chain_2 = prompt_restaurantes | modelo | parseador_restaurantes
+chain_3 = prompt_cultura | modelo | StrOutputParser()
+
+chain = ()
 
 
 response = chain.invoke(
