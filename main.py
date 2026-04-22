3fde4296c22c3b1dd5c8e64c8a91f916e8b4c97c
Diogo Hideyoshi <diohide12@gmail.com>
2026-04-22 15:13:34 -0300

    feat: add Pydantic BaseModel and Field for structured outputs and future integrations

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
+from langchain.globals import set_debug
 import os
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 
+class Destino(BaseModel):
+    cidade: str = Field("A cidade recomendada para visitar")
+    motivo: str = Field("O motivo pelo qual essa cidade é recomendada")
+
+parseador = JsonOutputParser(pydantic_object=Destino)
+
 prompt_cidade = PromptTemplate(
     template="""
     Sugira uma cidade dado o meu interesse por {interesse}.
+    {formato_de_saida}
     """,
     input_variables=["interesse"],
+    partial_variables={"formato_de_saida" : parseador.get_format_instructions() }
 )
 
 modelo = ChatOpenAI(
@@ -20,7 +30,7 @@ modelo = ChatOpenAI(
     api_key=api_key
 )
 
-chain = prompt_cidade | modelo | StrOutputParser()
+chain = prompt_cidade | modelo | parseador
 
 
 response = chain.invoke(
